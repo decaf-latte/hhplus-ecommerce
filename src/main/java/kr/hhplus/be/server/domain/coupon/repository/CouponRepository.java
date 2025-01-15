476f894a3e92aa5c647755c6f07471d34d5d0f6c
Hyejin <hyejin.jeon940120@gmail.com>
2025-01-16 01:41:40 +0900

    [REFACTORING] 쿠폰 발급 - 동시성제어 락 추가

--- src/main/java/kr/hhplus/be/server/domain/coupon/repository/CouponRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/repository/CouponRepository.java
@@ -1,7 +1,9 @@
 package kr.hhplus.be.server.domain.coupon.repository;
 
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.domain.coupon.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,6 @@ import java.util.Optional;
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Coupon> findByCode(String code);
 }
